e5a4a2378d93dafe2e39cf1cf3e62d229c88de02
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2024-03-08 00:57:02 +0900

    최초 커밋
    브랜치로 변경사항 관리예정

---
+++ src/main/java/com/example/testdrivenstudy/TestDrivenStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdrivenstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDrivenStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdrivenstudy/TestDrivenStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdrivenstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
